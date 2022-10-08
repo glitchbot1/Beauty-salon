cd6062dea77e13e7e72a73a403a20461f3bdf166
Anna Vladimirova <glitchbotmachine@gmail.com>
2022-10-08 14:28:48 +0700

    homework sprin2

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,46 @@ const { src, dest, parallel, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const clean = require('gulp-clean');
 const browserSync = require('browser-sync').create();
+const sourcemaps = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 
 
-function buildSass(){
+function buildSassDev(){
     return src('src/scss/**/*.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass({ includePaths: ['./node_modules'] }).on('error', sass.logError))
+    .pipe(
+        postcss([
+            autoprefixer({
+                grid: true,
+                overrideBrowserslist: ['last 2 versions']
+            }),
+            cssnano()
+        ])
+    )
+    .pipe(sourcemaps.write())
     .pipe(dest('src/css'))
     .pipe(dest('dist/css'))
     .pipe(browserSync.stream());
 }
+
+function buildSass(){
+    return src('src/scss/**/*.scss')
+    .pipe(sass({ includePaths: ['./node_modules'] }).on('error', sass.logError))
+    .pipe(
+        postcss([
+            autoprefixer({
+                grid: true,
+                overrideBrowserslist: ['last 2 versions']
+            }),
+            cssnano()
+        ])
+    )
+    .pipe(dest('dist/css'))
+}
+
 
 function buildHtml() {
     return src('src/**/*.html')
@@ -30,7 +61,7 @@ function cleanDist() {
 }
 
 function serve() {
-    watch('src/scss/**/*.scss', buildSass);
+    watch('src/scss/**/*.scss', buildSassDev);
     watch('src/**/*.html', buildHtml);
 }
 
@@ -46,4 +77,4 @@ exports.html = buildHtml;
 exports.copy = copy;
 
 exports.build = series(cleanDist, parallel(buildSass, buildHtml, copy));
-exports.default = series(buildSass, parallel(createDevServer, serve));
+exports.default = series(buildSassDev, parallel(createDevServer, serve));
