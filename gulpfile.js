1595c51e97c8411a99e08ddbc7e44947c33410b7
Anna Vladimirova <glitchbotmachine@gmail.com>
2022-10-24 19:12:38 +0700

    current status of the project

--- gulpfile.js
+++ gulpfile.js
@@ -1,88 +1,93 @@
-const { src, dest, parallel, watch, series } = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const clean = require('gulp-clean');
-const browserSync = require('browser-sync').create();
-const sourcemaps = require('gulp-sourcemaps');
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
+const { src, dest, parallel, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const clean = require("gulp-clean");
+const browserSync = require("browser-sync").create();
+const sourcemaps = require("gulp-sourcemaps");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const webpackStream = require("webpack-stream");
+const rename = require("gulp-rename");
 
+console.log(process.env.NODE_ENV);
 
-function buildSassDev(){
-    return src('src/scss/**/*.scss')
+function buildSassDev() {
+  return src("src/scss/**/*.scss")
     .pipe(sourcemaps.init())
-    .pipe(sass({ includePaths: ['./node_modules'] }).on('error', sass.logError))
+    .pipe(sass({ includePaths: ["./node_modules"] }).on("error", sass.logError))
     .pipe(
-        postcss([
-            autoprefixer({
-                grid: true,
-                overrideBrowserslist: ['last 2 versions']
-            }),
-            cssnano()
-        ])
+      postcss([
+        autoprefixer({
+          grid: true,
+          overrideBrowserslist: ["last 2 versions"],
+        }),
+        cssnano(),
+      ])
     )
     .pipe(sourcemaps.write())
-    .pipe(dest('src/css'))
-    .pipe(dest('dist/css'))
+    .pipe(dest("src/css"))
+    .pipe(dest("dist/css"))
     .pipe(browserSync.stream());
 }
 
-function buildSass(){
-    return src('src/scss/**/*.scss')
-    .pipe(sass({ includePaths: ['./node_modules'] }).on('error', sass.logError))
+function buildSass() {
+  return src("src/scss/**/*.scss")
+    .pipe(sass({ includePaths: ["./node_modules"] }).on("error", sass.logError))
     .pipe(
-        postcss([
-            autoprefixer({
-                grid: true,
-                overrideBrowserslist: ['last 2 versions']
-            }),
-            cssnano()
-        ])
+      postcss([
+        autoprefixer({
+          grid: true,
+          overrideBrowserslist: ["last 2 versions"],
+        }),
+        cssnano(),
+      ])
     )
-    .pipe(dest('dist/css'))
+    .pipe(dest("dist/css"));
 }
-
 
 function buildHtml() {
-    return src('src/**/*.html')
-    .pipe(dest('dist'))
+  return src("src/**/*.html").pipe(dest("dist")).pipe(browserSync.stream());
+}
+
+function buildJs() {
+  return src("src/js/main.js")
+    .pipe(webpackStream(require("./webpack.config")))
+    .pipe(rename("main.min.js"))
+    .pipe(dest("src/js"))
+    .pipe(dest("dist/js"))
     .pipe(browserSync.stream());
 }
 
-// function buildJs(){
-//     return gulp.src([
-//         'node_modules/slick-carousel/slick/slick.js'
-//       ])
-//     .pipe(dest('src/js'))
-//     .pipe(browserSync.stream());
-// }
-
 function copy() {
-    return src(['src/images/**/*.*'])
-    .pipe(dest('dist/images'));
+  return src(["src/images/**/*.*"]).pipe(dest("dist/images"));
 }
 
 function cleanDist() {
-    return src('dist', { allowEmpty: true })
-    .pipe(clean());
+  return src("dist", { allowEmpty: true }).pipe(clean());
 }
 
 function serve() {
-    watch('src/scss/**/*.scss', buildSassDev);
-    watch('src/**/*.html', buildHtml);
+  watch("src/scss/**/*.scss", buildSassDev);
+  watch("src/**/*.html", buildHtml);
+  watch(["src/js/**/*.js", "!src/js/**/*.min.js"], buildJs);
 }
 
 function createDevServer() {
-    browserSync.init({
-        server: 'src',
-        notify: false
-    })
+  browserSync.init({
+    server: "src",
+    notify: false,
+  });
 }
 
 exports.sass = buildSass;
 exports.html = buildHtml;
-// exports.html = buildJs;
 exports.copy = copy;
 
-exports.build = series(cleanDist, parallel(buildSass, buildHtml, copy));
-exports.default = series(buildSassDev, parallel(createDevServer, serve));
+exports.build = series(
+  cleanDist,
+  parallel(buildSass, buildHtml, copy, buildJs)
+);
+exports.default = series(
+  [buildSassDev, buildJs],
+  parallel(createDevServer, serve)
+);
